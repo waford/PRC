256e08bee1614dad5c2f470d3eb0083e5d082182
Wesley Ford <wesley.ford@gatech.edu>
2025-04-13 15:53:56 -0400

    Aded second board for occupied agents and refactord plotting code that should make it easier to display the board layers

--- src/env2.py
+++ src/env2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pdb
+import copy
 
 
 class Directions:
@@ -10,20 +11,23 @@ class Directions:
     LEFT = 4
 
 
+class AgentStates:
+    AGENT_FREE = 0
+    AGENT_OCCUPIED = 1
+
+
 MAX_ENERGY = 50
 IN_PATH_ENERGY = 2
-
-AGENT_FREE = 0
-AGENT_OCCUPIED = 1
 
 
 class Cell:
     def __init__(self, row, col):
         self.direction = Directions.NOP
         self.energy = 0.0
-        self.disapation = 0.9
+        self.disapation = 0.8
         self.row = row
         self.col = col
+        self.goal = False
 
     def is_aligned(self, other_cell):
         return self.direction == other_cell.direction
@@ -92,7 +96,7 @@ class Agent:
     def __init__(self, row, col):
         self.row = row
         self.col = col
-        self.state = AGENT_FREE
+        self.state = AgentStates.AGENT_FREE
 
     def desired_move(self, board):
         direction = board[self.row][self.col].direction
@@ -107,8 +111,6 @@ class Agent:
                 return (self.row, self.col - 1)
             case Directions.NOP:
                 return (self.row, self.col)
-
-        pdb.set_trace()
 
     def coord(self):
         return (self.row, self.col)
@@ -129,8 +131,10 @@ class Environment:
         self.sample_fraction = 0.80
         self.agents = []
 
-        self.board = [[Cell(row, col) for col in range(0, self.full_cols)]
-                      for row in range(0, self.full_rows)]
+        self.free_board = [[Cell(row, col) for col in range(0, self.full_cols)]
+                           for row in range(0, self.full_rows)]
+        self.occupied_board = copy.deepcopy(self.free_board)
+
         agent_coords_flat = np.random.choice(
             self.rows * self.cols, size=num_agents, replace=False)
 
@@ -146,36 +150,94 @@ class Environment:
         board = np.zeros((self.full_rows, self.full_cols))
         for row in range(0, self.full_rows):
             for col in range(0, self.full_cols):
-                board[row, col] = self.board[row][col].direction
+                board[row, col] = self.free_board[row][col].direction
 
         return board
 
-    def set_cell(self, row, col, direction, energy=0.0):
-        self.board[row][col].direction = direction
-        self.board[row][col].energy = energy
+    def set_cell(self, row, col, direction, energy=0.0, goal=False):
+        self.free_board[row][col].direction = direction
+        self.free_board[row][col].energy = energy
+        self.free_board[row][col].goal = goal
 
     def update_board(self):
-        new_board = list(self.board)
+        new_free_board = copy.deepcopy(self.free_board)
+        new_occupied_board = copy.deepcopy(self.occupied_board)
         for row in range(1, self.full_rows-1):
             for col in range(1, self.full_cols-1):
-                new_board[row][col].update_direction(self.board)
+                new_free_board[row][col].update_direction(self.free_board)
+                new_occupied_board[row][col].update_direction(
+                    self.occupied_board)
 
-        self.board = new_board
+        self.free_board = new_free_board
+        self.occupied_board = new_occupied_board
 
     def is_boundary(self, coord):
         row = coord[0]
         col = coord[1]
         return (row == 0) or (row == self.full_rows - 1) or (col == 0) or (col == self.full_cols - 1)
 
+    def is_goal(self, coord):
+        return self.free_board[coord[0]][coord[1]].goal
+
+    def get_cell(self, coord):
+        return self.free_board[coord[0]][coord[1]]
+
+    def is_north(self, coord):
+        return coord[0] == 0
+
+    def is_west(self, coord):
+        return coord[1] == 0
+
+    def is_south(self, coord):
+        return coord[0] == self.full_rows - 1
+
+    def is_east(self, coord):
+        return coord[1] == self.full_cols - 1
+
+    def random_goal(self):
+        rand_row = np.random.choice(self.full_rows - 1) + 1
+        rand_col = np.random.choice(self.full_cols - 1) + 1
+
+        options = [(0, rand_col), (self.full_rows - 1, rand_col),
+                   (rand_row, 0), (rand_row, self.full_cols - 1)]
+        goal_coord = options[np.random.choice(len(options))]
+        cell = self.get_cell(goal_coord)
+        if goal_coord[0] == 0:
+            cell.direction = Directions.UP
+        elif goal_coord[0] == self.full_rows-1:
+            cell.direction = Directions.DOWN
+        elif goal_coord[1] == 0:
+            cell.direction = Directions.RIGHT
+        elif goal_coord[1] == self.full_cols - 1:
+            cell.direction = Directions.LEFT
+
+        cell.energy = -10.0
+        cell.goal = True
+
     def update_agents(self):
-        # TODO: Allow agents to move into goal locations
         occupied = set()
         for agent in self.agents:
             occupied.add((agent.row, agent.col))
 
         for agent in self.agents:
-            move = agent.desired_move(self.board)
-            if not self.is_boundary(move) and (move not in occupied):
+            current_pos = agent.coord()
+            if self.is_goal(current_pos):
+                agent.state = AgentStates.AGENT_OCCUPIED
+                cell = self.get_cell(current_pos)
+                cell.energy = 0.0
+                cell.goal = False
+                if self.is_north(current_pos):
+                    cell.direction = Directions.DOWN
+                elif self.is_east(current_pos):
+                    cell.direction = Directions.LEFT
+                elif self.is_south(current_pos):
+                    cell.direction = Directions.UP
+                elif self.is_west(current_pos):
+                    cell.direction = Directions.RIGHT
+                continue
+
+            move = agent.desired_move(self.free_board)
+            if (not self.is_boundary(move) or self.is_goal(move)) and (move not in occupied):
                 old_location = agent.coord()
                 agent.update_location(move)
                 occupied.remove(old_location)
--- src/main.py
+++ src/main.py
@@ -1,48 +1,20 @@
+import pdb
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 import matplotlib.animation as animation
 from matplotlib.colors import ListedColormap, BoundaryNorm
-from env2 import Environment, Directions
-
-env = Environment(32, 32, num_agents=10, inv_temp=0.5)
-# env.board[0, 1:(env.full_cols-1)] = NOP
-# env.board[1:(env.full_rows - 1), -1] = NOP
-# env.board[-1, 1:(env.full_cols-1)] = NOP
-# env.board[1:(env.full_rows - 1), 0] = NOP
-#
-# env.board[env.full_rows - 1, 8] = DOWN
-# env.board[0, 8] = UP
-# env.board[8, 0] = LEFT
-# env.board[8, env.full_cols - 1] = RIGHT
-
-env.set_cell(env.full_rows - 1, 8, Directions.DOWN, energy=-10)
-env.set_cell(0, 8, Directions.UP, energy=-10)
-env.set_cell(8, 0, Directions.LEFT, energy=-10)
-env.set_cell(8, env.full_cols - 1, Directions.RIGHT, energy=-10)
-
-plt.rcParams['text.usetex'] = True
-
-fig, ax = plt.subplots()
-env.update_board()
+from env2 import Environment, Directions, AgentStates
 
 
-cm = ListedColormap(['w', 'g', 'purple', 'b', 'y'], N=5)
-ax_image = ax.matshow(env.get_board(), cmap=cm, vmin=0.0, vmax=4.0)
-ax.grid(which="minor", lw=2.0)
-ax.xaxis.set_minor_locator(MultipleLocator(base=1.0, offset=-0.5))
-ax.yaxis.set_minor_locator(MultipleLocator(base=1.0, offset=-0.5))
-
-X, Y = np.meshgrid(np.arange(0, env.full_rows), np.arange(0, env.full_cols))
-
-
-def create_quiver(X, Y, env):
+def create_quiver(X, Y, board):
     U = np.zeros_like(X)
     V = np.zeros_like(Y)
     mask = np.ones_like(U) == 1
-    board = env.get_board()
-    for row in range(env.full_rows):
-        for col in range(env.full_cols):
+    full_rows = len(board)
+    full_cols = len(board[0])
+    for row in range(full_rows):
+        for col in range(full_cols):
             value = board[row][col]
             if value == Directions.NOP:
                 mask[row, col] = False
@@ -61,50 +33,131 @@ def create_quiver(X, Y, env):
     return (U, V, mask)
 
 
-(U, V, mask) = create_quiver(X, Y, env)
-quiver = ax.quiver(X, Y, U, V)
-# agent_rows = [row for (row, _) in env.occupied]
-# agent_cols = [col for (_, col) in env.occupied]
+def agent_data(agents):
+    free_coords = []
+    occupied_coords = []
+    for agent in agents:
+        if agent.state == AgentStates.AGENT_FREE:
+            free_coords.append(agent.coord())
+        elif agent.state == AgentStates.AGENT_OCCUPIED:
+            occupied_coords.append(agent.coord())
+    return (np.array(free_coords), np.array(occupied_coords))
 
 
-agent_rows = []
-agent_cols = []
-for agent in env.agents:
-    agent_rows.append(agent.row)
-    agent_cols.append(agent.col)
+class BoardGraphic:
 
-data = np.stack([agent_rows, agent_cols]).T
-scat = ax.scatter(agent_cols, agent_rows, color="red")
+    def __init__(self, ax, board, agents):
+        cm = ListedColormap(['w', 'g', 'purple', 'b', 'y'], N=5)
+        self.ax = ax
+        self.ax_image = self.ax.matshow(board, cmap=cm, vmin=0.0, vmax=4.0)
+        self.ax.grid(which="minor", lw=2.0)
+        self.ax.xaxis.set_minor_locator(MultipleLocator(base=1.0, offset=-0.5))
+        self.ax.yaxis.set_minor_locator(MultipleLocator(base=1.0, offset=-0.5))
+        X, Y = np.meshgrid(np.arange(0, env.full_rows),
+                           np.arange(0, env.full_cols))
+        self.X = X
+        self.Y = Y
+        (U, V, mask) = create_quiver(X, Y, board)
+        self.quiver = self.ax.quiver(X, Y, U, V)
+
+        (free_agent_coords, occupied_agent_coords) = agent_data(env.agents)
+
+        if free_agent_coords.size > 0:
+            self.free_scat = self.ax.scatter(
+                free_agent_coords[:, 1], free_agent_coords[:, 0], color="red")
+        else:
+            self.free_agent_coords = self.ax.scatter([], [], color="red")
+
+        if occupied_agent_coords.size > 0:
+            self.occupied_scat = self.ax.scatter(
+                occupied_agent_coords[:, 1], occupied_agent_coords[:, 0], color="cyan")
+        else:
+            self.occupied_scat = self.ax.scatter([], [], color="cyan")
+
+    def update_board(self, board):
+        (U, V, mask) = create_quiver(self.X, self.Y, env.get_board())
+        U = np.ma.array(U, mask=np.logical_not(mask))
+        V = np.ma.array(V, mask=np.logical_not(mask))
+        self.quiver.set_UVC(U, V)
+        self.ax_image.set(data=board)
+
+    def update_agents(self, agents):
+        (free_data, occupied_data) = agent_data(agents)
+
+        # free_data = np.stack([free_agent_cols, free_agent_rows]).T
+        # occupied_data = np.stack([occupied_agent_cols, occupied_agent_rows]).T
+        if free_data.size != 0:
+            self.free_scat.set_offsets(free_data[:, ::-1])
+        else:
+            self.free_scat.set_offsets(np.stack([[], []]).T)
+
+        if occupied_data.size != 0:
+            self.occupied_scat.set_offsets(occupied_data[:, ::-1])
+        else:
+            self.occupied_scat.set_offsets(np.stack([[], []]).T)
+
+
+env = Environment(32, 32, num_agents=40, inv_temp=0.5)
+# env.board[0, 1:(env.full_cols-1)] = NOP
+# env.board[1:(env.full_rows - 1), -1] = NOP
+# env.board[-1, 1:(env.full_cols-1)] = NOP
+# env.board[1:(env.full_rows - 1), 0] = NOP
+#
+# env.board[env.full_rows - 1, 8] = DOWN
+# env.board[0, 8] = UP
+# env.board[8, 0] = LEFT
+# env.board[8, env.full_cols - 1] = RIGHT
+
+env.set_cell(env.full_rows - 1, 8, Directions.DOWN, energy=-10, goal=True)
+# env.set_cell(0, 8, Directions.UP, energy=-10, goal=True)
+# env.set_cell(8, 0, Directions.LEFT, energy=-10, goal=True)
+# env.set_cell(8, env.full_cols - 1, Directions.RIGHT, energy=-10, goal=True)
+
+# env.random_goal()
+# env.random_goal()
+# env.random_goal()
+# env.random_goal()
+# env.random_goal()
+
+# plt.rcParams['text.usetex'] = True
+
+fig, ax = plt.subplots()
+env.update_board()
+
+bg = BoardGraphic(ax, env.get_board(), env.agents)
+
+#
+#
+#cm = ListedColormap(['w', 'g', 'purple', 'b', 'y'], N=5)
+#ax_image = ax.matshow(env.get_board(), cmap=cm, vmin=0.0, vmax=4.0)
+#ax.grid(which="minor", lw=2.0)
+#ax.xaxis.set_minor_locator(MultipleLocator(base=1.0, offset=-0.5))
+#ax.yaxis.set_minor_locator(MultipleLocator(base=1.0, offset=-0.5))
+
+X, Y = np.meshgrid(np.arange(0, env.full_rows), np.arange(0, env.full_cols))
 
 
 def fig_update(frame):
     # inv_temp = 0.85 + frame * 0.0025
     # env.inv_temp = inv_temp
+    if frame % 10 == 0:
+        env.random_goal()
+
     if frame % 2 == 0:
         env.update_board()
     else:
         env.update_agents()
+
     board = env.get_board()
-    (U, V, mask) = create_quiver(X, Y, env)
-    U = np.ma.array(U, mask=np.logical_not(mask))
-    V = np.ma.array(V, mask=np.logical_not(mask))
-    quiver.set_UVC(U, V)
-    ax_image.set(data=board)
+    bg.update_board(board)
+    bg.update_agents(env.agents)
 
-    agent_rows = []
-    agent_cols = []
-    for agent in env.agents:
-        agent_rows.append(agent.row)
-        agent_cols.append(agent.col)
-
-    data = np.stack([agent_cols, agent_rows]).T
-    scat.set_offsets(data)
-
-    ax.set_title(r"Inv Temp={:.2f}, Done: {} ".format(
-        env.inv_temp, env.is_done()))
-    return ax_image
+    bg.ax.set_title(r"t: {}, Done: {} ".format(
+        frame, env.is_done()))
+    return bg.ax_image
 
 
+frames = 100
 ani = animation.FuncAnimation(
-    fig=fig, func=fig_update, frames=400, interval=100)
+    fig=fig, func=fig_update, frames=frames, interval=100)
 ani.save("32x32.gif")
